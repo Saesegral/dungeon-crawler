e321f89b03e138e41952787e376e653e800e0f42
Saesegral <stevenkgrigsby@gmail.com>
2018-10-17 07:04:57 -0500

    Animated Basic Character
    Added Standard Assets and Example 5 from NavMesh animation tutorial, have some extra things floating around I believe, like extra .blend files with some shape keys.

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -2,23 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class PlayerController : MonoBehaviour {
 
     public Camera cam;
     public NavMeshAgent agent;
-	
-	// Update is called once per frame
-	void Update () {
+    public ThirdPersonCharacter character;
+
+    private void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+#elif (UNITY_ANDROID || UNIT_IOS) && !UNITY_EDITOR
+            //Consider doing something more involved for clicking on enemies and items or UI later.
+        if (Input.touchCount > 0)
+        {
+            Ray ray = cam.ScreenPointToRay(Input.touches[0].position);
+#endif
             RaycastHit hit;
 
             if(Physics.Raycast(ray, out hit))
             {
                 agent.SetDestination(hit.point);
             }
+        }
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
         }
 	}
 }
