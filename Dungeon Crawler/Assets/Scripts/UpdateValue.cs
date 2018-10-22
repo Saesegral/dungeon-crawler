ccf98d49049ac6279c2f68d7e200b23c39c36221
Saesegral <stevenkgrigsby@gmail.com>
2018-10-22 13:21:16 -0500

    Laid out GUI
    Used a lot of placeholders, not a lot of real functionality or actual things here.

---
+++ Dungeon Crawler/Assets/Scripts/UpdateValue.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UpdateValue : MonoBehaviour {
+
+    private TextMeshProUGUI tmp;
+
+    private void Start() {
+        tmp = GetComponent<TextMeshProUGUI>();
+        UpdateText(0.0f);
+    }
+    
+    public void UpdateText(float f) {
+        tmp.text = f.ToString();
+    }
+}
---
+++ Dungeon Crawler/Assets/UpdateValue.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UpdateValue : MonoBehaviour {
+
+    private TextMeshProUGUI tmp;
+
+    private void Start() {
+        tmp = GetComponent<TextMeshProUGUI>();
+        //Default value, might be better to make a reference to the slider/button and set the default.
+        UpdateText(0.0f);
+    }
+    
+    public void UpdateText(float f) {
+        tmp.text = f.ToString();
+    }
+}
